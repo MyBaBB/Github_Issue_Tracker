f8908071c11eff7824c074d53b226462b348a226
Brett Baker <GitBaBB@MyBaBB.com>
2024-03-28 11:58:05 -0700

    test the kitty

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,9 @@ function App() {
         </Routes>
         
       </Router>
-      <FaGithub />
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <FaGithub size={50} className="text-blue-500 dark:text-gray-200 bg-grey-500 dark:bg-blue-900 " />
+      </div>
       <Footer />
     </div>
   );
