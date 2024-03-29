388a25ae3041f1da5055661f74516372837afb10
Brett Baker <GitBaBB@MyBaBB.com>
2024-03-29 05:34:28 -0700

    typo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ function About() {
       <h2 className="text-3xl underline decoration-green-500 font-semibold text-center">About Issue Tracker</h2>
       <p className="text-2xl mt-3 px-20 text-justify">
         A simple and effective application to keep track of GitHub issues that
-        I&apos;ve applied for.
+        have been  applied for. Quick and Easy mobile reference.
       </p>
       <p className="text-xl mt-3 text-center mb-4">
         Created with Productivity in Mind &nbsp;
