7883910b6dc41e3785faa13779dd0bd262764566
Brett Baker <GitBaBB@MyBaBB.com>
2024-06-18 12:33:35 -0700

    changed app name in vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,54 +1,85 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+ 
+ 
+ 
 
+
+ 
 export default defineConfig({
   base: './', 
+
   plugins: [
     react(),
     VitePWA({
       manifest: {
-        "name":"Issue tracker",
+        "name":"Issue Tracker",
         "short_name":"Tracker",
         "start_url":"./",
         "display":"standalone",
         "background_color":"#2a61a1",
         "lang":"en","scope":"./",
-      },
-        icons: [
-          
+        "icons":[
           {
-            src: 'maskable192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: "any maskable",
-      },
-          {
-            src: 'logo192.jpg',
-            sizes: '192x192',
-            type: 'image/png',
-           
-      },
-      
-          {
-            src: 'logo256.jpg',
-            sizes: '256x256',
-            type: 'image/png',
-       
-      },
-          {
-            src: 'logo384.jpg',
-            sizes: '384x384',
-            type: 'image/png',
-          
-          },
-          {
-            src: 'logo512.jpg',
-            sizes: '512x512',
-            type: 'image/png',
-           
-          },
-        ],
+            "src":"favicon-32x32.png",
+             "sizes":"32x32","type":"image/png",
+             "purpose":"any maskable"
+            },
+             {
+              "src":"favicon-16x16.png",
+              "sizes":"16x16",
+              "type":"image/png",
+              "purpose":"any maskable"
+            },
+              {
+                "src":"maskable192.png",
+                "sizes":"192x192","type":"image/png",
+                "purpose":"any maskable"
+            },
+              {
+                "src":"maskable192.png",
+                "sizes":"192x192","type":"image/png",
+                "purpose":"any maskable"
+            },
+              {
+                "src":"logo192.jpg",
+              "sizes":"192x192",
+              "type":"image/jpg"
+            },
+              {
+                "src":"logo256.jpg",
+                "sizes":"256x256",
+                "type":"image/jpg"
+            },
+              {
+                "src":"logo384'jpg",
+                "sizes":"384x384",
+                "type":"image/jpg"
+            },
+              {
+                "src":"logo512.jpg",
+                "sizes":"512x512",
+                "type":"image/jpg"
+            },
+              {
+                "src":"android-chrome-512x512.png",
+                "sizes":"512x512","type":"image/png",
+                "purpose":"any maskable"
+            },
+              {
+                "src":"android-chrome-192x192.png",
+                "sizes":"192x192","type":"image/png",
+                "purpose":"any maskable"
+            },
+              {
+                "src":"apple-touch-icon.png",
+                "sizes":"180x180","type":"image/png",
+                "purpose":"any maskable"
+            }],
+              "theme_color":"#0d2657"
+            }
+
     }),
   ],
 })
